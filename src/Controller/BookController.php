01c3745ee8b60d1d0c27bce8fe7b6346cf71d73d
Fabrice Piedanna <rs45959@gmail.com>
2025-08-19 14:45:29 +0200

    update book controller add create and delete

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Book;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface as EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface as Serializer;
 
 final class BookController extends AbstractController
@@ -32,5 +35,28 @@ final class BookController extends AbstractController
             true
         );
 
+    }
+
+    #[Route('/api/books/{id}', name: 'deleteBook', methods: ['DELETE'])]
+    public function deleteBook(Book $book, EntityManager $em): JsonResponse
+    {
+        $em->remove($book);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/books', name: 'createBook', methods: ['POST'])]
+    public function createBook(Request $request, EntityManager $em, UrlGeneratorInterface $urlGenerator, Serializer $serializer): JsonResponse
+    {
+        $book = $serializer->deserialize($request->getContent(), Book::class, 'json');
+        $em->persist($book);
+        $em->flush();
+
+        $jsonBook = $serializer->serialize($book, 'json', ['groups' => ['getBooks']]);
+        $location = $urlGenerator->generate('detailBook', ['id' =>
+        $book->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonBook, Response::HTTP_CREATED, ['Location' => $location], true);
     }
 }

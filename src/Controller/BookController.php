b78b1d04826bbb9948c35017b2b535df35e39dfa
Fabrice Piedanna <rs45959@gmail.com>
2025-08-19 11:40:59 +0200

    add fixtures, bookController, symfony/framework-bundle, serializer

---
+++ src/Controller/BookController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface as Serializer;
+
+final class BookController extends AbstractController
+{
+    #[Route('/api/books', name: 'book', methods: ['GET'])]
+    public function getBookList(BookRepository $bookRepository, Serializer $serializer): JsonResponse
+    {
+        $booklist = $bookRepository->findAll();
+        $jsonBookList = $serializer->serialize($booklist, 'json');
+
+        return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/books/{id}', name: 'detailBook', methods: ['GET'])]
+    public function getDetailBook(Book $book, Serializer $serializer): JsonResponse
+    {
+        $jsonBook = $serializer->serialize($book, 'json');
+        return new JsonResponse(
+            $jsonBook,
+            Response::HTTP_OK,
+            [],
+            true
+        );
+
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Book;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // Création d'une vingtaine de livres ayant pour titre
+        for ($i = 0; $i < 20; $i++) {
+            $livre = new Book();
+            $livre->setTitle('Livre ' . $i);
+            $livre->setCoverText('Quatrième de couverture numéro :
+' . $i);
+            $manager->persist($livre);
+        }
+        $manager->flush();
+    }
+}

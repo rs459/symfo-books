bf1fb5b22c5779edf5e004c5b1864eed8a519a9a
Fabrice Piedanna <rs45959@gmail.com>
2025-08-22 15:31:47 +0200

    create User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,34 @@ namespace App\DataFixtures;
 
 use App\Entity\Book;
 use App\Entity\Author;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
+
+        //Création Users
+        $user = new User();
+        $user->setEmail('user@bookapi.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+        $manager->persist($user);
+
+        // Création d'un administrateur
+        $admin = new User();
+        $admin->setEmail('admin@bookapi.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->userPasswordHasher->hashPassword($admin, 'password'));
+        $manager->persist($admin);
 
         // Création Auteurs
         $listAuthors = [];
